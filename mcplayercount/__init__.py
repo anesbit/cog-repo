d7856d649d51c7bc9d570f912c1af00d3c118e9b
BuildTools <unconfigured@null.spigotmc.org>
2020-10-13 19:01:18 -0500

    basic cog more will come...

---
+++ mcplayercount/__init__.py
@@ -0,0 +1,4 @@
+from .mycog import mcplayercount
+
+def setup(bot):
+    bot.add_cog(mcplayercount())
---
+++ mcplayercount/mcplayercount.py
@@ -0,0 +1,8 @@
+from redbot.core import commands
+
+class mcplayercount(commands.Cog):
+
+    @commands.command()
+    async def mycom(self, ctx):
+
+        await ctx.send('I can do things!')
